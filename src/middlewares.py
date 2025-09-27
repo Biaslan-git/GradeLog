eb7627425aac8c1da3a5079ecaeacdfd06df9481
django <django.ppp@gmail.com>
2025-09-27 19:32:42 +0300

    Увеличил глубины вывода ошибки в телеграм сообщении

--- src/middlewares.py
+++ src/middlewares.py
@@ -6,11 +6,12 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 def error_handler(func):
     @wraps(func)
-    async def wrapper(message: types.Message, *args, **kwargs):
+    async def wrapper(message_or_callback: types.Message | types.CallbackQuery, *args, **kwargs):
         try:
-            await func(message, *args, **kwargs)  # Без return
+            await func(message_or_callback, *args, **kwargs)  # Без return
         except Exception as e:
-            error_message = traceback.format_exc(limit=1)
+            message = message_or_callback if type(message_or_callback) == types.Message else message_or_callback.message
+            error_message = traceback.format_exc(limit=2)
 
             error_message = f'Произошла ошибка:\n{message.chat.id}\n{func.__name__}: {error_message}'
 
