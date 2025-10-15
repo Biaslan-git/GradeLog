57acc50ff1d0a221b191ddd2b1afea6971ff0857
django <django.ppp@gmail.com>
2025-10-15 21:15:24 +0300

    feat(bot commands): register bot commands in menu

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import asyncio
 import logging
 
-from aiogram import Dispatcher
+from aiogram.types import BotCommand
 
 from src.handlers import routers
 from src.bot import bot
 from src.dp import dp
 
 logging.basicConfig(level=logging.INFO)
+
+
+async def set_commands(bot):
+    commands = [
+        BotCommand(command="start", description="Запустить бота"),
+    ]
+    await bot.set_my_commands(commands)
+
+@dp.startup()
+async def on_startup(bot):
+    await set_commands(bot)
+    print("Команды установлены!")
+
+
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
